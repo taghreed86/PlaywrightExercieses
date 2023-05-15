941e1616b6a0027a3944262e5cae320e5ba87195
taghreed86 <tagreed_23@live.com>
2023-05-15 22:02:16 +0300

    restructure the files to use page object model

---
+++ test_WGE/test_Policies_Applications.py
@@ -0,0 +1,68 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+from pages.policies_page import Policies
+from pages.application_page import Applications
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False, slow_mo=1000)
+    context = browser.new_context()
+    page = context.new_page()
+    page.wait_for_load_state("networkidle")
+    page.set_default_timeout(10000)
+    # page.pause()
+    page.goto("https://demo-01.wge.dev.weave.works/sign_in")
+    # page.get_by_placeholder("Username").click()
+    # page.get_by_placeholder("Username").fill("wego-admin")
+    # playwright.selectors.set_test_id_attribute("id")
+    # page.get_by_test_id("email").click()
+    page.locator("id=email").click()
+    page.locator("id=email").fill("wego-admin")
+    page.get_by_placeholder("Password").click()
+    page.get_by_placeholder("Password").fill(">ch2yU5@]F8U6IZkX?Q#")
+    page.get_by_role("button", name="CONTINUE").click()
+    expect(page).to_have_url("https://demo-01.wge.dev.weave.works/clusters/list")
+
+    policies_page = Policies(page)
+    applications_page = Applications(page)
+
+    def test_open_policies_page():
+        policies_page.open_policies_page()
+        expect(page).to_have_url("https://demo-01.wge.dev.weave.works/policies")
+
+    def test_open_policy_details_page():
+        policies_page.open_policy_details_page()
+        expect(page).to_have_url("https://demo-01.wge.dev.weave.works/policies/details?clusterName=management&id=weave.policies.containers-minimum-replica-count")
+
+    def test_open_applications_page():
+        applications_page.open_application_page()
+        expect(page).to_have_url("https://demo-01.wge.dev.weave.works/applications")
+
+    def test_open_application_details_page():
+        applications_page.open_application_details_page()
+        expect(page).to_have_url("https://demo-01.wge.dev.weave.works/kustomization/details?clusterName=management&name=canaries&namespace=flux-system")
+
+    def test_open_application_yaml():
+        applications_page.open_application_yaml_tab()
+        expect(page.get_by_text("kubectl get kustomization canaries -n flux-system -o yaml")).to_be_visible()
+
+    # page.pause()
+    def test_open_application_violations_page():
+        applications_page.open_application_violations_tab()
+        expect(page).to_have_url("https://demo-01.wge.dev.weave.works/kustomization/violations?clusterName=management&name=canaries&namespace=flux-system")
+
+    def test_open_application_violations_details():
+        applications_page.open_application_violations_details()
+        assert "https://demo-01.wge.dev.weave.works/violations/details?clusterName=management&id=" in page.url
+        expect(page.locator("text=Containers Minimum Replica Count in deployment podinfo (1 occurrences)")).to_be_visible()
+
+    def test_open_policy_details_from_app_violations_details_page():
+        applications_page.open_policy_details_from_application_violations_details_page()
+        expect(page.locator("text=weave.policies.containers-minimum-replica-count")).to_be_visible()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
