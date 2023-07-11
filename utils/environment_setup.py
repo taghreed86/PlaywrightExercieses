10284616e0904468cc2be0f6912170b1d3f5ced5
taghreed86 <tagreed_23@live.com>
2023-07-11 21:37:59 +0300

    add sh scripts files

---
+++ utils/environment_setup.py
@@ -0,0 +1,11 @@
+
+KindMgmtCluster = "kind"
+EKSMgmtCluster  = "eks"
+GKEMgmtCluster  = "gke"
+
+WGE_WINDOW_NAME = "weave-gitops-enterprise"
+GITOPS_DEFAULT_NAMESPACE = "flux-system"
+CLUSTER_SERVICE_DEPLOYMENT_APP = "my-mccp-cluster-service"
+SCREENSHOTS_DIR_NAME = "screenshots"
+
+# def setup_test_environment():
